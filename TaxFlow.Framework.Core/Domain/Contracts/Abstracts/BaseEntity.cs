ed31872beff49736f7d12014f81661cbdb0104d5
Daniel WOAGOU <pwoagou@otr.tg>
2025-12-03 12:31:42 +0000

    Refactorisation et ajout d'attributs extensibles
    - Ajout des dossiers `Domain\Tax\Enums\` et `Domain\Workflow\` dans `Core.csproj`.
    - Déplacement de la classe `BaseEntity<TId>` dans un nouveau fichier `BaseEntity.cs`.
    - Déplacement de la classe `SoftAuditableEntity` dans un nouveau fichier `SoftAuditableEntity.cs`.
    - Ajout de la classe `AuditableEntityWithExtendableAttribute` pour gérer des attributs extensibles.
    - Suppression des espaces de noms inutilisés dans `AssetType.cs` et `TaxableAsset.cs`.
    - Réorganisation et nettoyage des espaces de noms pour améliorer la lisibilité et la modularité.

---
+++ TaxFlow.Framework.Core/Domain/Contracts/Abstracts/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using Core.Domain.Contracts.Event;
+
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Domain.Contracts.Abstracts;
+
+public abstract class BaseEntity<TId> : IEntity<TId>
+{
+    public TId Id { get; protected init; } = default!;
+    [NotMapped]
+    public Collection<IDomainEvent> DomainEvents { get; } = new Collection<IDomainEvent>();
+    public void QueueDomainEvent(IDomainEvent @event)
+    {
+        if (!DomainEvents.Contains(@event))
+            DomainEvents.Add(@event);
+    }
+}
---
+++ TaxFlow.Framework.Core/Domain/Contracts/Abstracts/SoftAuditableEntity.cs
@@ -0,0 +1,26 @@
+﻿using Core.Domain.Contracts.Event;
+
+namespace Core.Domain.Contracts.Abstracts;
+
+public abstract class SoftAuditableEntity: AuditableEntity, ISoftDeletable
+{
+    public DateTimeOffset? Deleted { get; private set; }
+    public Guid? DeletedBy { get; private set; }
+    public DateTimeOffset? LastDeletedOn { get; private set; }
+    public Guid? LastDeletedby { get; private set; }
+    public DateTimeOffset? LastRecovered { get; private set; }
+    public Guid? LastRecoveredBy { get; private set; }
+    public void Recover(Guid recoveredBy)
+    {       
+        LastDeletedOn = Deleted;
+        LastDeletedby = DeletedBy;        
+        LastRecovered = DateTimeOffset.UtcNow;
+        LastRecoveredBy = recoveredBy;
+        // Clear soft-delete markers; audit fields updated by infrastructure
+        Deleted = null;
+        DeletedBy = null;
+        QueueDomainEvent(new EntityRecoveredDomainEvent<Guid>(Id, recoveredBy));
+    }
+
+
+}
